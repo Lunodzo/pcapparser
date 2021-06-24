2ec82b01869938b621643c6566443979812d2abd
lunodzo <lunomwinuka@gmail.com>
2021-06-24 15:03:54 +0300

    first commit

---
+++ src/main/java/com/javahelps/App.java
@@ -0,0 +1,13 @@
+package com.javahelps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
